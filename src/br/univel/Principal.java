1f055b0e52811fa611f75af1998f5e2802bd03e2
Fabio <kucinskifk@gmail.com>
2016-03-15 20:20:04 -0300

    Alteração a pedido do cliente no passado versão 1.0

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -8,8 +8,8 @@ package br.univel;
 
 public class Principal {
 	 public static void main(String[] args) {
-		System.out.println("olá Mundo!");
-		System.out.println("É nóis!");
+		System.out.println("hello world!");
+		System.out.println("java comanda");
 		
 	}
 }
