6c570b92ddc8fa634871ce18f3711671febc0e31
Fabio <kucinskifk@gmail.com>
2016-03-15 19:58:48 -0300

    Liberação de versão

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -1,5 +1,11 @@
 package br.univel;
 
+/**
+ * versão 1.0
+ * @author Administrador
+ *
+ */
+
 public class Principal {
 	 public static void main(String[] args) {
 		System.out.println("olá Mundo!");
